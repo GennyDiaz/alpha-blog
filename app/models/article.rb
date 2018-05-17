1eaccf679b6bf7cd06a628d92a0ada6fa58d880f
Rails Udemy Course <john.nicholai.vigilia@gmail.com>
2018-05-17 13:19:34 +0800

    added association between user and article and debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 
 class Article < ActiveRecord::Base
+	belongs_to :user
 	#ensures the title and description are true with min and max limit
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 	validates :description, presence: true, length: { minimum: 10, maximum: 300 }  
+	validates :user_id, presence: true
 end
